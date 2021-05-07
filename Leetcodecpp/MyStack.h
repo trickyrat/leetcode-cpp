3290ebece8c7cc76f664fcd2bae5548339dca025
trickyrat <wangjian0513@outlook.com>
2021-05-07 15:58:29 +0800

    add mystack

---
+++ Leetcodecpp/MyStack.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <queue>
+
+class MyStack {
+public:
+  MyStack() {}
+
+  void push(int x) {
+    int n = q.size();
+    q.push(x);
+    for (int i = 0; i < n; i++) {
+      q.push(q.front());
+      q.pop();
+    }
+  }
+
+  int pop() {
+    int r = q.front();
+    q.pop();
+    return r;
+  }
+
+  int top() {
+    int r = q.front();
+    return r;
+  }
+
+  bool empty() { 
+    return q.empty();
+  }
+private:
+  std::queue<int> q;
+};
