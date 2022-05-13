1e052223b89b249aa9b97e600cb63b680f282aa9
trickyrat <wangjian0513@outlook.com>
2022-05-14 01:18:47 +0800

    refactored test codes

---
+++ Leetcodecpp.Test/AllOneUnitTest.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+#include "../Leetcodecpp/AllOne.cpp"
+#include "../Leetcodecpp/AllOne.h"
+
+using namespace std;
+
+TEST(AllOneTest, OperationTest) {
+  AllOne allOne;
+  allOne.inc("hello");
+  allOne.inc("hello");
+  string actualMaxKey1 = allOne.getMaxKey();
+  string expected1 = "hello";
+  EXPECT_EQ(expected1, actualMaxKey1);
+  string actualMinKey1 = allOne.getMinKey();
+  string expected2 = "hello";
+  EXPECT_EQ(expected2, actualMinKey1);
+  allOne.inc("leet");
+  string actualMaxKey2 = allOne.getMaxKey();
+  string expected3 = "hello";
+  EXPECT_EQ(expected3, actualMaxKey2);
+  string actualMinKey2 = allOne.getMinKey();
+  string expected4 = "leet";
+  EXPECT_EQ(expected4, actualMinKey2);
+}
