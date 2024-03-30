03e60c8859793a5cd8b76ac8dc3f582692a3a130
trickyrat <wangjian0513@outlook.com>
2024-03-31 00:37:52 +0800

    test: add TwoSumUnitTest

---
+++ LeetCode.Tests/TwoSumUnitTest.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+
+#include "../LeetCode/Solution.h"
+
+using namespace std;
+
+class TwoSumFixture {
+public:
+  vector<int> nums;
+  int target;
+  vector<int> expected;
+
+  TwoSumFixture(vector<int> nums, int target, vector<int> expected)
+      : nums(nums), target(target), expected(expected){};
+};
+
+class TwoSumTest : public testing::TestWithParam<TwoSumFixture> {};
+
+INSTANTIATE_TEST_CASE_P(TwoSumTestCases, TwoSumTest,
+                        testing::Values(TwoSumFixture({2, 7, 11, 15}, 9,
+                                                      {0, 1}),
+                                        TwoSumFixture({3, 2, 4}, 6, {1, 2}),
+                                        TwoSumFixture({3, 3}, 6, {0, 1})));
+
+TEST_P(TwoSumTest, Test) {
+  TwoSumFixture param = GetParam();
+  Solution solution;
+  vector<int> actual = solution.two_sum(param.nums, param.target);
+  EXPECT_EQ(param.expected, actual);
+}
