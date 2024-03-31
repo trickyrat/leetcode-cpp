45673a31dcfd2dcbe5f9db0b8a8e3f5de3d430c0
trickyrat <wangjian0513@outlook.com>
2024-03-31 12:15:31 +0800

    test: Add AddTwoNumbersUnitTest and CodecUnitTest

---
+++ LeetCode.Tests/CodecUnitTest.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+
+#include "../LeetCode/codec.h"
+#include "../LeetCode/utils.h"
+
+using namespace std;
+
+class CodecFixture {
+public:
+  string node;
+  string expected;
+
+  CodecFixture(string node, string expected) : node(node), expected(expected){};
+};
+
+class CodecTest : public testing::TestWithParam<CodecFixture> {};
+
+INSTANTIATE_TEST_CASE_P(CodecTestCases, CodecTest,
+                        testing::Values(CodecFixture("1,2,3,null,null,4,5",
+                                                     "1,2,3,null,null,4,5"),
+                                        CodecFixture("", "")));
+
+TEST_P(CodecTest, Test) {
+  CodecFixture param = GetParam();
+  TreeNode *root = Utils::generate_tree_node(param.node);
+  Codec serializer, deserializer;
+  TreeNode *actual = deserializer.deserialize(serializer.serialize(root));
+  TreeNode *expected = Utils::generate_tree_node(param.expected);
+  EXPECT_TRUE(Utils::is_same_tree(expected, actual));
+}
